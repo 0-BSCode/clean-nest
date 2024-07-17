a40a02a2730808ad60655172ad9d9adb5e537aa6
0-BSCode <bryanipsanchez@gmail.com>
2024-07-17 15:56:44 +0800

    Add logger

--- src/filters/http-exception/http-exception.filter.ts
+++ src/filters/http-exception/http-exception.filter.ts
@@ -3,14 +3,24 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  Logger,
 } from '@nestjs/common';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private logger = new Logger(HttpExceptionFilter.name);
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const status = exception.getStatus();
+
+    const errorObject = {
+      message: exception.message,
+      status,
+      stack: exception?.stack,
+    };
+
+    this.logger.error(JSON.stringify(errorObject));
 
     const errorResponse = {
       statusCode: status,
@@ -18,6 +28,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
       path: ctx.getRequest().url,
       message: exception.message,
     };
+
     response.status(status).json(errorResponse);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 // TODO: Set up CORS, swagger, helmet, pipes
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const PORT = 4000;
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
